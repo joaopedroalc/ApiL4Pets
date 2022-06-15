295439f418404e7a496a51b1a422530f86f561f5
joaopedroalc <joaopedro.ufma@gmail.com>
2022-06-14 21:30:27 -0300

    database name

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '12345678',
-    database: 'lookingforpets'
+    database: 'LookingForPets'
 });
 
 module.exports = pool;
