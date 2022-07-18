9cb4bb8b45f207f982a3a3fc22129fe6a137e0b8
joaopedroalc <joaopedro.ufma@gmail.com>
2022-07-18 17:03:52 -0300

    configs do heroku clear db e ajuste no mysql

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 var pool = mysql.createPool({
-    host: 'localhost',
-    user: 'joaopedrocode',
-    password: '347542',
-    database: 'LookingForPets'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bf001df63aee3e',
+    password: '0c2d9bee',
+    database: 'heroku_e6d60efc1cc8183'
 });
 
 module.exports = pool;
